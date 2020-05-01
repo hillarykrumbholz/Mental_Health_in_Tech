e562c09dd74ec735d264dddb4c2c4d948cca0096
hillarykrumbholz <hkrumbho@gmail.com>
2020-04-30 20:51:16 -0700

    Adding flask app folder and code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require("path")
 
 const app = express();
 
 //  This will change I am just getting node and express to work
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname,"./index.html"));
 });
 
 app.listen(3000,function(){

1d59a2c91ae0b75deb0b6b7493bc85a26c685eac
hillarykrumbholz <hkrumbho@gmail.com>
2020-05-01 23:16:15 -0700

    updating html

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,12 @@
 console.log("Waddup")
+
+//Tableau Embed function
+function initViz() {
+    url = "https://public.tableau.com/profile/rudy7195#!/vizhome/FP_COO/Sheet1",
+    options = {
+        hideToolbar: true,
+        width: "100%",
+        height: "200px",
+    };
+    viz = new tableau.Viz(tabMonthlySales, url, options);
+  }

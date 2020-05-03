c3d3662bbc0e57a5a16bae228af79551c9485dff
unknown <hackbarth.jd@gmail.com>
2020-05-03 16:26:54 -0700

    added questions

--- static/js/hillary.js
+++ static/js/hillary.js
@@ -2,9 +2,13 @@ const oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
 oReq.open("GET", "/api/v1.0/data");
 oReq.send();
+
+
 const darkModeButton = document.querySelector(".dark-mode-button");
 const visList = document.querySelectorAll(".vis");
 const dropdownMenu = document.querySelector(".dropdown-menu");
+const table = document.querySelector("tbody");
+
 darkModeButton.addEventListener("click", (event) => {
     const button = event.target
     if (button.classList.contains("btn-dark")) {
@@ -12,11 +16,14 @@ darkModeButton.addEventListener("click", (event) => {
         button.classList.add("btn-light");
         button.innerHTML = "Light Mode";
         document.body.style.backgroundColor = "black";
+
     } else {
         button.classList.remove("btn-light");
         button.classList.add("btn-dark");
         button.innerHTML = "Dark Mode";
         document.body.style.backgroundColor = "white";
+
+
     }
 })
 function dropDown() {
@@ -42,6 +49,31 @@ function showVis(visText) {
     }
 }
 function reqListener() {
-    console.log(this.responseText);
+
+
+    const json = JSON.parse(this.response);
+    buildTable(json["data"]);
+    
+
 }
+
+function buildTable(json) {
+
+    html = '';
+
+    for (let row of json) {
+        
+        console.log(row);
+        html += 
+        `
+            <tr>
+            <th scope="row">${row[0] + 1}</th>
+            <td>${row[1]}</td>
+            </tr>
+        `
+    }
+    
+    table.innerHTML = html;
+}
+
 dropDown();
